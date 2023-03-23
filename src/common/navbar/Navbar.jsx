1fa08d531219032f8ea4d1f7278ec6223f83067d
“Ruslan <russ_o@ukr.net>
2023-03-23 15:50:28 +0200

    hammenu

--- src/common/navbar/Navbar.jsx
+++ src/common/navbar/Navbar.jsx
@@ -1,10 +1,19 @@
+
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState } from 'react';
 import {NavLink} from 'react-router-dom';
 import './navbar.scss'
 const Navbar = () => {
+
+    const [openNav, setOpenNav] = useState(false)
     return (
         <div className='navbar'>
             <div className="container navbar__container">
-                <ul className='navbar__items'>
+                <ul className={
+                    openNav ? ["navbar__items", "navbar__active"].join(" ") : 
+                    ["navbar__items"]}
+                    >
                     <li className='navbar__item'>
                         <NavLink to='/' >HOME</NavLink>
                     </li>
@@ -18,6 +27,13 @@ const Navbar = () => {
                         <NavLink to='/nasa-image' >NASA IMAGE</NavLink>
                     </li>
                 </ul>
+                <div className="navbar__mobile-btn" onClick={()=> setOpenNav(!openNav)}>
+                
+                 {openNav ? <FontAwesomeIcon icon={faXmark} className="navbar__mobile-close" /> : <FontAwesomeIcon  icon={faBars} className="navbar__mobile-menu" />}
+                </div>
+               
+               
+                
             </div>
         </div>
     );
